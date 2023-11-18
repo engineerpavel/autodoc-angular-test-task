0ea2cc2f8f96feb11fefbb115a19152cb632c453
Pavel Pogorelov <engineer.pavel@yandex.ru>
2023-11-18 19:22:40 +0300

    заготовка для компонентов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {NewsFeedComponent} from "./components/news-feed/news-feed.component";
+import {NewsDetalComponent} from "./components/news-detal/news-detal.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NewsFeedComponent },
+  { path: 'news', component: NewsDetalComponent },
+];
---
+++ src/app/components/news-detal/news-detal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-news-detal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './news-detal.component.html',
+  styleUrl: './news-detal.component.scss'
+})
+export class NewsDetalComponent {
+
+}
