a52ef629488a2d7985399acbe7334c7d8d3efc0a
Pavel Pogorelov <engineer.pavel@yandex.ru>
2023-11-18 22:54:43 +0300

    компонент карточки

--- src/app/components/news-feed/news-feed.component.ts
+++ src/app/components/news-feed/news-feed.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-news-feed',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './news-feed.component.html',
   styleUrl: './news-feed.component.scss'
 })
