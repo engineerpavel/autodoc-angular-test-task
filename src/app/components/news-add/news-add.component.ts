915767df68bc74d3848314c71206cd429e9fcd55
Pavel Pogorelov <47284402+engineerpavel@users.noreply.github.com>
2023-11-29 22:54:34 +0300

    Feature/image upload (#7)
    * получаем base64 из выбранного изображения;
    
    * внезапная запятая
    
    * превью изображения, WIP
    
    * bugfix: повторное открытие того же файла
    
    * сохранение новости в локалсторадж
    
    * валидация формы перед отправкой

--- src/app/components/news-add/news-add.component.ts
+++ src/app/components/news-add/news-add.component.ts
@@ -1,15 +1,86 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, inject, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImagePreviewComponent } from '../image-preview/image-preview.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NewsFeedInterface } from '../../models/news.interface';
 
 @Component({
   selector: 'app-news-add',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImagePreviewComponent, ReactiveFormsModule],
   templateUrl: './news-add.component.html',
   styleUrl: './news-add.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewsAddComponent {
   @Output()
-  close = new EventEmitter<void>;
+  close = new EventEmitter<void>();
+  cdr = inject(ChangeDetectorRef);
+  preview = '';
+  newsForm = new FormGroup({
+    title: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    text: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    image: new FormControl('', { nonNullable: true, validators: [Validators.required] })
+  });
+
+  private readonly newsKey = 'newsFeed';
+
+  /**
+   * Обработчик выбора изображения
+   * @param event
+   */
+  selectImage(event: Event): void {
+    const imgFiles = (event.target as HTMLInputElement).files;
+    if (imgFiles) {
+      const imgFile = imgFiles.item(0);
+      if (imgFile) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.preview = e.target.result;
+          this.cdr.markForCheck();
+        };
+        reader.readAsDataURL(imgFile);
+      }
+    }
+  }
+
+  /**
+   * Удалить выбранное изображение
+   */
+  onDeletePreview(): void {
+    this.preview = '';
+    this.newsForm.get('image')?.reset();
+  }
+
+  /**
+   * Опубликовать новость
+   * (добавить в LocalStorage)
+   */
+  publish(): void {
+    if (this.newsForm.valid) {
+      let newsFeedString = localStorage.getItem(this.newsKey);
+      let newsFeedArr: NewsFeedInterface[] = [];
+      if (newsFeedString) {
+        try {
+          newsFeedArr = JSON.parse(newsFeedString);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+
+      newsFeedArr.push({
+        categoryType: '',
+        description: '',
+        fullUrl: '',
+        id: 0,
+        publishedDate: Date.now().toString(),
+        title: this.newsForm.get('title')!.value,
+        titleImageUrl: this.preview,
+        url: ''
+      });
+
+      localStorage.setItem(this.newsKey, JSON.stringify(newsFeedArr));
+      this.close.emit();
+    }
+  }
 }
