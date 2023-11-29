fb400c022fcb9d1e587b25553dda551f05914d5f
Pavel Pogorelov <engineer.pavel@yandex.ru>
2023-11-29 13:54:17 +0300

    получаем base64 из выбранного изображения;

--- src/app/components/news-add/news-add.component.ts
+++ src/app/components/news-add/news-add.component.ts
@@ -1,5 +1,5 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-news-add',
@@ -11,5 +11,26 @@ import {CommonModule} from '@angular/common';
 })
 export class NewsAddComponent {
   @Output()
-  close = new EventEmitter<void>;
+  close = new EventEmitter<void>();
+  preview = '';
+
+  /**
+   * Обработчик выбора изображения
+   * @param event
+   */
+  selectImage(event: Event): void {
+    const imgFiles = (event.target as HTMLInputElement).files;
+    if (imgFiles) {
+      const imgFile = imgFiles.item(0);
+      if (imgFile) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          console.log(e.target.result);
+          this.preview = e.target.result;
+        };
+        reader.readAsDataURL(imgFile);
+        console.log('загрузили файл');
+      }
+    }
+  }
 }
